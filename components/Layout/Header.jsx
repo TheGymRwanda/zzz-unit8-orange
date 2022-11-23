cdf2747b917a5151b70160744d690f1a492b90ac
Jacob Dev <mercihabam@gmail.com>
2022-11-23 16:12:28 +0200

    feat: add header in layout component and apply the layout component to the whole project

--- components/Layout/Header.jsx
+++ components/Layout/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Wrapper from '../sections/Wrapper'
 
 const Header = () => {
   return (
-    <div className='py-12 flex-col lg:flex-row gap-5 flex text-2xl justify-between'>
-        <div className="">Ape Unit</div>
-        <div className="max-w-201.5">Unit➇ is a pioneering technology company specialing in decentralised technologies that creates end-to-end digital experiences for protocols including <a href="#" className='text-purple'>Ethereum</a>, <a href="#" className='text-purple'>Tezos</a>, <a href="#" className='text-purple'>Near</a>, <a href="#" className='text-purple'>Algorand</a>, <a href="#" className='text-purple'>Celo</a> and more. Its team of 2,500+ digital specialists across 30+ locations on 5 continents delivers pioneering work on a global scale with a boutique culture. </div>
-    </div>
+    <Wrapper>
+        <div className='py-12 flex-col lg:flex-row gap-5 flex text-2xl justify-between'>
+            <div className="">Ape Unit</div>
+            <div className="max-w-201.5">Unit➇ is a pioneering technology company specialing in decentralised technologies that creates end-to-end digital experiences for protocols including <a href="#" className='text-purple'>Ethereum</a>, <a href="#" className='text-purple'>Tezos</a>, <a href="#" className='text-purple'>Near</a>, <a href="#" className='text-purple'>Algorand</a>, <a href="#" className='text-purple'>Celo</a> and more. Its team of 2,500+ digital specialists across 30+ locations on 5 continents delivers pioneering work on a global scale with a boutique culture. </div>
+        </div>
+    </Wrapper>
   )
 }
 
