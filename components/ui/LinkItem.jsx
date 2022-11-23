ba195d1b25217d78cb23dab8f1fb3b27aa058d71
Jacob Dev <mercihabam@gmail.com>
2022-11-23 16:25:37 +0200

    fix: create icons folder and add inside the missing icon for the link component

--- components/ui/LinkItem.jsx
+++ components/ui/LinkItem.jsx
@@ -5,7 +5,7 @@ const LinkItem = ({children}) => {
     <div className='flex items-center gap-1 group cursor-pointer'>
         {children}
         <div className="w-max group-hover:translate-x-1 group-hover:-translate-y-1 transition-all">
-            <Image width={14} height={14} src="/assets/icons/arrow-link.png" alt='arrow' />
+            <Image width={14} height={14} src="/assets/icons/arrow-link.svg" alt='arrow' />
         </div>
     </div>
   )
