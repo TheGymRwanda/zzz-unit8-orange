82a0165c058a8df70dd08cf82d34699cec825cc5
Jacob Dev <mercihabam@gmail.com>
2022-11-23 16:49:05 +0200

    fix: change font on small screen & change anchor tag with next-link tag

--- components/Layout/Header.jsx
+++ components/Layout/Header.jsx
@@ -1,12 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 import Wrapper from '../sections/Wrapper'
 
 const Header = () => {
   return (
     <Wrapper>
-        <div className='py-12 flex-col lg:flex-row gap-5 flex text-2xl justify-between'>
+        <div className='py-12 flex-col lg:flex-row gap-5 flex text-xl sm:text-2xl justify-between -tracking-tighter sm:tracking-normal'>
             <div className="">Ape Unit</div>
-            <div className="max-w-201.5">Unit➇ is a pioneering technology company specialing in decentralised technologies that creates end-to-end digital experiences for protocols including <a href="#" className='text-purple'>Ethereum</a>, <a href="#" className='text-purple'>Tezos</a>, <a href="#" className='text-purple'>Near</a>, <a href="#" className='text-purple'>Algorand</a>, <a href="#" className='text-purple'>Celo</a> and more. Its team of 2,500+ digital specialists across 30+ locations on 5 continents delivers pioneering work on a global scale with a boutique culture. </div>
+            <div className="max-w-201.5">Unit➇ is a pioneering technology company specialing in decentralised technologies that creates end-to-end digital experiences for protocols including <Link href="#" className='text-purple'>Ethereum</Link>, <Link href="#" className='text-purple'>Tezos</Link>, <Link href="#" className='text-purple'>Near</Link>, <Link href="#" className='text-purple'>Algorand</Link>, <Link href="#" className='text-purple'>Celo</Link> and more. Its team of 2,500+ digital specialists across 30+ locations on 5 continents delivers pioneering work on a global scale with a boutique culture. </div>
         </div>
     </Wrapper>
   )
