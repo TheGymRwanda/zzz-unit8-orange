36931d3e5e315c0e1a63304002e6eb27debce04c
Jacob Dev <mercihabam@gmail.com>
2022-11-23 19:57:41 +0200

    fix: add contactUs link in header & fix linkItem component width'

--- components/Layout/Header.jsx
+++ components/Layout/Header.jsx
@@ -1,12 +1,13 @@
 import Link from 'next/link'
 import Wrapper from '../sections/Wrapper'
+import LinkItem from '../ui/LinkItem'
 
 const Header = () => {
   return (
     <Wrapper>
-        <div className='py-12 flex-col lg:flex-row gap-5 flex text-xl sm:text-2xl justify-between -tracking-tighter sm:tracking-normal'>
+        <div className='py-12 flex-col lg:flex-row gap-5 sm:gap-0 flex text-xl sm:text-2xl justify-between -tracking-tighter sm:tracking-normal'>
             <div>Ape Unit</div>
-            <div className="max-w-201.5">Unit➇ is a pioneering technology company specialing in decentralised technologies that creates end-to-end digital experiences for protocols including <Link href="#" className='text-purple'>Ethereum</Link>, <Link href="#" className='text-purple'>Tezos</Link>, <Link href="#" className='text-purple'>Near</Link>, <Link href="#" className='text-purple'>Algorand</Link>, <Link href="#" className='text-purple'>Celo</Link> and more. Its team of 2,500+ digital specialists across 30+ locations on 5 continents delivers pioneering work on a global scale with a boutique culture. </div>
+            <div className="max-w-201.5">Unit➇ is a pioneering technology company specialing in decentralised technologies that creates end-to-end digital experiences for protocols including <Link href="#" className='text-purple'>Ethereum</Link>, <Link href="#" className='text-purple'>Tezos</Link>, <Link href="#" className='text-purple'>Near</Link>, <Link href="#" className='text-purple'>Algorand</Link>, <Link href="#" className='text-purple'>Celo</Link> and more. Its team of 2,500+ digital specialists across 30+ locations on 5 continents delivers pioneering work on a global scale with a boutique culture. <LinkItem>Contact Us</LinkItem></div>
         </div>
     </Wrapper>
   )
--- components/ui/LinkItem.jsx
+++ components/ui/LinkItem.jsx
@@ -3,12 +3,10 @@ import Link from 'next/link';
 
 const LinkItem = ({children, to}) => {
   return (
-    <Link href={to || '#'}>
-        <div className='flex items-center gap-1 group cursor-pointer'>
-            {children}
-            <div className="w-max group-hover:translate-x-1 group-hover:-translate-y-1 transition-all">
-                <Image width={14} height={14} src="/assets/icons/arrow-link.svg" alt='arrow' />
-            </div>
+    <Link href={to || '#'} className="w-max flex items-center gap-1 group cursor-pointer">
+        {children}
+        <div className="w-max group-hover:translate-x-1 group-hover:-translate-y-1 transition-all">
+            <Image width={14} height={14} src="/assets/icons/arrow-link.svg" alt='arrow' />
         </div>
     </Link>
   )
