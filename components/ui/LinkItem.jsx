f7679ab0ca7133e0d43cf236aedc1e3384fb8d27
Jacob Dev <mercihabam@gmail.com>
2022-11-23 17:20:07 +0200

    fix: wrap link item content in next-link component

--- components/ui/LinkItem.jsx
+++ components/ui/LinkItem.jsx
@@ -1,13 +1,16 @@
 import Image from 'next/image'
+import Link from 'next/link';
 
-const LinkItem = ({children}) => {
+const LinkItem = ({children, to}) => {
   return (
-    <div className='flex items-center gap-1 group cursor-pointer'>
-        {children}
-        <div className="w-max group-hover:translate-x-1 group-hover:-translate-y-1 transition-all">
-            <Image width={14} height={14} src="/assets/icons/arrow-link.svg" alt='arrow' />
+    <Link href={to || '#'}>
+        <div className='flex items-center gap-1 group cursor-pointer'>
+            {children}
+            <div className="w-max group-hover:translate-x-1 group-hover:-translate-y-1 transition-all">
+                <Image width={14} height={14} src="/assets/icons/arrow-link.svg" alt='arrow' />
+            </div>
         </div>
-    </div>
+    </Link>
   )
 }
 
