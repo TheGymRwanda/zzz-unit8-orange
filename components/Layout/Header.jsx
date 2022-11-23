2a1ccf8a1b1af013fb7a3a43a929eafba06c0b21
Jacob Dev <mercihabam@gmail.com>
2022-11-23 16:06:18 +0200

    feat: create header section component

---
+++ components/Layout/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='py-12 flex-col lg:flex-row gap-5 flex text-2xl justify-between'>
+        <div className="">Ape Unit</div>
+        <div className="max-w-201.5">Unit➇ is a pioneering technology company specialing in decentralised technologies that creates end-to-end digital experiences for protocols including <a href="#" className='text-purple'>Ethereum</a>, <a href="#" className='text-purple'>Tezos</a>, <a href="#" className='text-purple'>Near</a>, <a href="#" className='text-purple'>Algorand</a>, <a href="#" className='text-purple'>Celo</a> and more. Its team of 2,500+ digital specialists across 30+ locations on 5 continents delivers pioneering work on a global scale with a boutique culture. </div>
+    </div>
+  )
+}
+
+export default Header
