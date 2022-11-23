7e317a38840fd80168576ef2bf64eef6431468ed
alain-kubwayo <ahkubwayo@gmail.com>
2022-11-23 18:12:19 +0200

    feat: add font size and letter spacing to the title on mobile screen

--- components/ui/Title.jsx
+++ components/ui/Title.jsx
@@ -1,3 +1,3 @@
-const Title = ({ name }) => <h3 className="text-2xl leading-7 text-primaryGray-300 -tracking-tighter">{ name }</h3>
+const Title = ({ name }) => <h3 className="text-xl leading-7 -tracking-wider sm:text-2xl sm:-tracking-tighter text-primaryGray-300">{ name }</h3>
 
 export default Title;
