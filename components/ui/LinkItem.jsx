64ac80d7d9c635e6ef5d9a16d224929d48e267bf
Jacob Dev <mercihabam@gmail.com>
2022-11-24 11:44:29 +0200

    fix: change commas color & link-item content alignement

--- components/ui/LinkItem.jsx
+++ components/ui/LinkItem.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const LinkItem = ({children, to}) => {
   return (
-    <Link href={to || '#'} className="w-max flex items-center gap-1 group cursor-pointer">
+    <Link href={to || '#'} className="w-max flex items-baseline gap-1 group cursor-pointer">
         {children}
         <div className="w-max group-hover:translate-x-1 group-hover:-translate-y-1 transition-all">
             <Image width={14} height={14} src="/assets/icons/arrow-link.svg" alt='arrow' />
