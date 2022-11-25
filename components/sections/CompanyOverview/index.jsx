55d88940985cc7c1ba17588dcc963467960ddb1c
Odile078 <ouwimpuhwe620@daviscollege.com>
2022-11-25 12:56:44 +0200

    feat: Add CompanyOverview component - mobile screen

--- components/sections/CompanyOverview/index.jsx
+++ components/sections/CompanyOverview/index.jsx
@@ -1,10 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import ArrowIcon from '../../ui/ArrowIcon';
 import Wrapper from '../Wrapper';
 import Overview from './Overview';
 const CompanyOverview = () => {
     const [activeTab, setActiveTab] = useState("about");
-    const changeActiveTab =(tab)=>tab === "about"? setActiveTab("about"):tab === "what we do"? setActiveTab("what we do"): tab === "hiring" ? setActiveTab("hiring") : null;   
-    const overview = activeTab==="about"?"Blockchain will do for cooperation what the internet did for communication - We believe that blockchain technology has the potential to shift financial models as we know them, creating revenue streams that are more sustainable and fair for users. We envision a future in which financial infrastructures are rather decentralised and horizontal, in which any member can have a say. Through its immutability, blockchain technology provides trustworthy infrastructures that allow cooperation on a large scale.":activeTab==="what we do"?"Blockchain will do for cooperation what the internet did for communication - We believe that blockchain technology has the potential to shift financial models as we know them, creating revenue streams that are more sustainable and fair for users. We envision a future in which financial infrastructures are rather decentralised and horizontal, in which any member can have a say. Through its immutability, blockchain technology provides trustworthy infrastructures that allow cooperation on a large scale.":activeTab==="hiring"?"Blockchain will do for cooperation what the internet did for communication - We believe that blockchain technology has the potential to shift financial models as we know them, creating revenue streams that are more sustainable and fair for users. We envision a future in which financial infrastructures are rather decentralised and horizontal, in which any member can have a say. Through its immutability, blockchain technology provides trustworthy infrastructures that allow cooperation on a large scale.":""
+    const changeActiveTab =(tab)=>tab === "about"? setActiveTab("about"):tab === "what we do"? setActiveTab("what we do"): tab === "hiring" ? setActiveTab("hiring") : null; 
+    const changeActiveTabMobile =(tab)=>{
+        if(tab==="about"){
+            activeTab==="about"?setActiveTab(null):setActiveTab("about")
+        }else if(tab==="what we do"){
+            activeTab==="what we do"?setActiveTab(null):setActiveTab("what we do")
+        }
+        else if(tab==="hiring"){
+            activeTab==="hiring"?setActiveTab(null):setActiveTab("hiring")
+        }
+    }
+    const overview = activeTab==="about"?"Blockchain will do for cooperation what the internet did for communication - We believe that blockchain technology has the potential to shift financial models as we know them, creating revenue streams that are more sustainable and fair for users. We envision a future in which financial infrastructures are rather decentralised and horizontal, in which any member can have a say. Through its immutability, blockchain technology provides trustworthy infrastructures that allow cooperation on a large scale.":activeTab==="what we do"?"Blocks will do for cooperation what the internet did for communication - We believe that blockchain technology has the potential to shift financial models as we know them, creating revenue streams that are more sustainable and fair for users. We envision a future in which financial infrastructures are rather decentralised and horizontal, in which any member can have a say. Through its immutability, blockchain technology provides trustworthy infrastructures that allow cooperation on a large scale.":activeTab==="hiring"?"We believe that blockchain technology has the potential to shift financial models as we know them, creating revenue streams that are more sustainable and fair for users. We envision a future in which financial infrastructures are rather decentralised and horizontal, in which any member can have a say. Through its immutability, blockchain technology provides trustworthy infrastructures that allow cooperation on a large scale.":""
+    const handleResize = () =>{
+        if(window.innerWidth>= 640&&!activeTab) {
+            console.log("innerWidth is larger than 640: "+ window.innerWidth)
+            setActiveTab("about")
+        }
+    }
+    useEffect(()=>{
+        window.addEventListener("resize", handleResize)
+        return ()=> window.removeEventListener("resize", handleResize)
+    },[])
     return (
         <Wrapper>
             <div className="justify-end hidden sm:flex">
@@ -16,6 +37,38 @@ const CompanyOverview = () => {
                     </ul>
                     <Overview paragraph={overview}/>
                 </div>
+            </div>
+            <div className="block sm:hidden">
+                <ul className='space-y-5.1'>
+                    <li className="space-y-4.5">
+                        <div onClick={()=>changeActiveTabMobile("about")} className={"flex items-center justify-between border-b "+ (activeTab==="about"?"border-purple":"border-primaryGray-300")}>
+                            <p className={"cursor-default text-xl -tracking-wider "+(activeTab==="about"?"text-purple ":"")}>About us</p>
+                            <ArrowIcon variant={activeTab==="about"? " text-purple rotate-180 ": " text-black "}/>
+                        </div>
+                        {activeTab==="about"&&<div className='pb-8.4'>
+                            <Overview paragraph={overview}/>
+                         </div>
+                         }
+                    </li>
+                    <li className="space-y-4.5">
+                        <div onClick={()=>changeActiveTabMobile("what we do")} className={" flex items-center justify-between border-b "+ (activeTab==="what we do"?"border-purple":"border-primaryGray-300")}>
+                            <p className={"cursor-default text-xl -tracking-wider "+(activeTab==="what we do"?"text-purple ":"")}>What we do</p>
+                            <ArrowIcon variant={activeTab==="what we do"? " text-purple rotate-180 ": " text-black "}/>
+                        </div>
+                        {activeTab==="what we do"&&<div className='pb-8.4'>
+                            <Overview paragraph={overview}/>
+                        </div>}                  
+                    </li>
+                    <li className="space-y-4.5">
+                        <div onClick={()=>changeActiveTabMobile("hiring")} className={" flex items-center justify-between border-b "+ (activeTab==="employment"?"border-purple":"border-primaryGray-300")}>
+                            <p className={"cursor-default text-xl -tracking-wider "+(activeTab==="hiring"?"text-purple ":"")}>Employment</p>
+                            <ArrowIcon variant={activeTab==="hiring"? " text-purple rotate-180 ": " text-black "}/>
+                        </div>
+                        {activeTab==="hiring"&&<div className='pb-8.4'>
+                            <Overview paragraph={overview}/>
+                        </div>}
+                    </li>
+                </ul>
             </div>
         </Wrapper>
     );
---
+++ components/ui/ArrowIcon.jsx
@@ -0,0 +1,10 @@
+const ArrowIcon = ({variant}) => {
+    return (
+    <svg width="19" height="19" viewBox="0 0 19 19" fill="none" className={variant} xmlns="http://www.w3.org/2000/svg">
+        <path d="M15.8594 10.4862L8.53726 17.8083L1.21514 10.4862" stroke="currentColor" strokeWidth="1.5"/>
+        <line x1="8.62402" y1="17.8082" x2="8.62402" y2="0.235152" stroke="currentColor" strokeWidth="1.5"/>
+    </svg> 
+    );
+}
+
+export default ArrowIcon;
