ea59b8c37e5a0baea4646c895ea2d2f6b35450f6
Liliane Iradukunda <Lialsina03@gmail.com>
2022-11-28 21:01:49 +0200

    feat: Add tab animation on mobile and larger screens

--- components/sections/CompanyOverview/index.jsx
+++ components/sections/CompanyOverview/index.jsx
@@ -2,77 +2,88 @@ import { useEffect, useState } from 'react';
 import ArrowIcon from '../../ui/ArrowIcon';
 import Wrapper from '../Wrapper';
 import Overview from './Overview';
-const CompanyOverview = () => {
-    const [activeTab, setActiveTab] = useState("about");
-    const changeActiveTab =(tab)=>tab === "about"? setActiveTab("about"):tab === "what we do"? setActiveTab("what we do"): tab === "hiring" ? setActiveTab("hiring") : null; 
-    const changeActiveTabMobile =(tab)=>{
-        if(tab==="about"){
-            activeTab==="about"?setActiveTab(null):setActiveTab("about")
-        }else if(tab==="what we do"){
-            activeTab==="what we do"?setActiveTab(null):setActiveTab("what we do")
-        }
-        else if(tab==="hiring"){
-            activeTab==="hiring"?setActiveTab(null):setActiveTab("hiring")
-        }
+import LinkItem from './../../ui/LinkItem';
+import {motion as Motion, AnimatePresence } from 'framer-motion';
+const data = [
+    {
+        title: "About",
+        text: <>
+            We believe that blockchain technology has the potential to shift financial models as we know them, creating revenue streams that are more sustainable and fair for users. We envision a future in which financial infrastructures are rather decentralised and horizontal, in which any member can have a say. Through its immutability, blockchain technology provides trustworthy infrastructures that allow cooperation on a large scale.{" "}
+            <div className="inline-block underline hover:no-underline underline-offset-2"><LinkItem to="https://medium.com/@moritzfelipe/blockchain-the-internet-for-cooperation-37a606bb3c0" underlined>Learn more</LinkItem></div>
+        </>,
+        subText: <>Work Meaningful - With our work, we aim to impact society positively. The projects we get involved with the aim to solve significant problems in our society, sustainably and in the long run, rather than looking for a quick profit through speculation or encouraging harmful behavior.</>
+    },
+    {
+        title: "What we do",
+        text: <>
+           Blocks will do for cooperation what the internet did for communication - We believe that blockchain technology has the potential to shift financial models as we know them, creating revenue streams that are more sustainable and fair for users. We envision a future in which financial infrastructures are rather decentralised and horizontal, in which any member can have a say. Through its immutability, blockchain technology provides trustworthy infrastructures that allow cooperation on a large scale.
+           <div className="inline-block underline hover:no-underline underline-offset-2"><LinkItem to="https://medium.com/@moritzfelipe/blockchain-the-internet-for-cooperation-37a606bb3c0" underlined>Learn more</LinkItem></div>
+        </>,
+        subText: <>Work Meaningful - With our work, we aim to impact society positively. The projects we get involved with the aim to solve significant problems in our society, sustainably and in the long run, rather than looking for a quick profit through speculation or encouraging harmful behavior.</>
+    },
+    {
+        title: "Hiring",
+        text: <>
+           Blocks will do for cooperation what the internet did for communication - We believe that blockchain technology has the potential to shift financial models as we know them, creating revenue streams that are more sustainable and fair for users. We envision a future in which financial infrastructures are rather decentralised and horizontal, in which any member can have a say. Through its immutability, blockchain technology provides trustworthy infrastructures that allow cooperation on a large scale.
+           <div className="inline-block underline hover:no-underline underline-offset-2"><LinkItem to="https://medium.com/@moritzfelipe/blockchain-the-internet-for-cooperation-37a606bb3c0" underlined> Learn more</LinkItem></div>
+        </>,
+        subText: <>Work Meaningful - With our work, we aim to impact society positively. The projects we get involved with the aim to solve significant problems in our society, sustainably and in the long run, rather than looking for a quick profit through speculation or encouraging harmful behavior.</>
     }
-    const overview = activeTab==="about"?"Blockchain will do for cooperation what the internet did for communication - We believe that blockchain technology has the potential to shift financial models as we know them, creating revenue streams that are more sustainable and fair for users. We envision a future in which financial infrastructures are rather decentralised and horizontal, in which any member can have a say. Through its immutability, blockchain technology provides trustworthy infrastructures that allow cooperation on a large scale.":activeTab==="what we do"?"Blocks will do for cooperation what the internet did for communication - We believe that blockchain technology has the potential to shift financial models as we know them, creating revenue streams that are more sustainable and fair for users. We envision a future in which financial infrastructures are rather decentralised and horizontal, in which any member can have a say. Through its immutability, blockchain technology provides trustworthy infrastructures that allow cooperation on a large scale.":activeTab==="hiring"?"We believe that blockchain technology has the potential to shift financial models as we know them, creating revenue streams that are more sustainable and fair for users. We envision a future in which financial infrastructures are rather decentralised and horizontal, in which any member can have a say. Through its immutability, blockchain technology provides trustworthy infrastructures that allow cooperation on a large scale.":""
-   
+];
+const CompanyOverview = () => {
+    const [activeTab, setActiveTab] = useState(data[0]);
+    const [activeTabIndex, setActiveTabIndex] = useState(0);
+    const changeActiveTabMobile =(tab)=> tab===activeTabIndex?setActiveTabIndex(null):setActiveTabIndex(tab)
     useEffect(()=>{
         const handleResize = () =>{
-            if(window.innerWidth>= 640&&!activeTab) {
-                console.log("innerWidth is larger than 640: "+ window.innerWidth)
-                setActiveTab("about")
-            }
-        }
+            if(window.innerWidth>= 640&&!activeTabIndex){
+                setActiveTab(data[0])
+                setActiveTabIndex(0)
+            } 
+        } 
         window.addEventListener("resize", handleResize)
         return ()=> window.removeEventListener("resize", handleResize)
     },[activeTab])
     return (
         <Wrapper>
-            <div className="justify-end hidden mt-24 sm:flex ">
-                <div className="space-y-5 sm:w-full lg:w-757 xl:w-811">
-                    <ul className="flex gap-10.5 items-center py-5">
-                        <li onClick={()=>changeActiveTab("about")} className={"cursor-default text-2xl leading-extra -tracking-tighter transition-all ease-in-out duration-300 "+(activeTab==="about"?"text-purple underline underline-offset-4":"text-primaryGray-300")}><p>About us</p></li>
-                        <li onClick={()=>changeActiveTab("what we do")} className={"cursor-default text-2xl leading-extra -tracking-tighter transition-all ease-in-out duration-300 "+(activeTab==="what we do"?"text-purple underline underline-offset-4":"text-primaryGray-300")}><p>What we do</p></li>
-                        <li onClick={()=>changeActiveTab("hiring")} className={"cursor-default text-2xl leading-extra -tracking-tighter transition-all ease-in-out duration-300 "+(activeTab==="hiring"?"text-purple underline underline-offset-4":"text-primaryGray-300")}><span className="hidden lg:block">Hiring</span><span className=" lg:hidden">Employment</span></li>
+            <div className="hidden sm:flex justify-end mt-24">
+                <div className="sm:w-full lg:w-757 xl:w-811">
+                    <ul className="flex gap-10.5 items-center py-5 relative">
+                        {
+                            data.map((item, index) =>(
+                                <li className="space-y-5 relative" key={index}>
+                                    <button className={"cursor-pointer text-2xl leading-extra -tracking-tighter transition-all ease-in-out duration-300 "+(index===activeTabIndex?" text-purple ": " text-primaryGray-300 ")} key={index} onClick={() =>{setActiveTabIndex(index); setActiveTab(item)}} id={index}>
+                                      {item.title}
+                                    </button>
+                                    {index===activeTabIndex?(<Motion.div className="absolute mt-0 bottom-1 inset-x-0 h-0.2 bg-purple" layoutId="underline"/>):null}
+                                </li>
+                            ))
+                        }
                     </ul>
-                    <Overview paragraph={overview}/>
+                    <Overview paragraph={activeTab} />
                 </div>
-            </div>
+            </div>          
             <div className="block sm:hidden">
                 <ul className='space-y-5.1'>
-                    <li className="space-y-4.5">
-                        <div onClick={()=>changeActiveTabMobile("about")} className={"flex items-center justify-between border-b "+ (activeTab==="about"?"border-purple":"border-primaryGray-300")}>
-                            <p className={"cursor-default text-xl -tracking-wider "+(activeTab==="about"?"text-purple ":"")}>About us</p>
-                            <ArrowIcon variant={activeTab==="about"? " text-purple rotate-180 ": " text-black "}/>
-                        </div>
-                        {activeTab==="about"&&<div className='pb-8.4'>
-                            <Overview paragraph={overview}/>
-                         </div>
-                         }
-                    </li>
-                    <li className="space-y-4.5">
-                        <div onClick={()=>changeActiveTabMobile("what we do")} className={" flex items-center justify-between border-b "+ (activeTab==="what we do"?"border-purple":"border-primaryGray-300")}>
-                            <p className={"cursor-default text-xl -tracking-wider "+(activeTab==="what we do"?"text-purple ":"")}>What we do</p>
-                            <ArrowIcon variant={activeTab==="what we do"? " text-purple rotate-180 ": " text-black "}/>
-                        </div>
-                        {activeTab==="what we do"&&<div className='pb-8.4'>
-                            <Overview paragraph={overview}/>
-                        </div>}                  
-                    </li>
-                    <li className="space-y-4.5">
-                        <div onClick={()=>changeActiveTabMobile("hiring")} className={" flex items-center justify-between border-b "+ (activeTab==="employment"?"border-purple":"border-primaryGray-300")}>
-                            <p className={"cursor-default text-xl -tracking-wider "+(activeTab==="hiring"?"text-purple ":"")}>Employment</p>
-                            <ArrowIcon variant={activeTab==="hiring"? " text-purple rotate-180 ": " text-black "}/>
-                        </div>
-                        {activeTab==="hiring"&&<div className='pb-8.4'>
-                            <Overview paragraph={overview}/>
-                        </div>}
-                    </li>
+                    {
+                        data.map((item, index) =>
+                            <li className="space-y-4.5" key={index}>
+                                <div onClick={()=>{changeActiveTabMobile(index);setActiveTab(item)}} className={"flex items-center justify-between border-b "+ (activeTabIndex===index?"border-purple":"border-primaryGray-300")}>
+                                    <p className={"cursor-default text-xl -tracking-wider "+(activeTabIndex===index?"text-purple ":"")}> {item.title}</p>
+                                    <div className={`transition-all duration-300 ease-out ${activeTabIndex===index? "rotate-180": ""}`}>
+                                        <ArrowIcon variant={activeTabIndex===index? " text-purple ": " text-black "}/>
+                                    </div>
+                                </div>
+                                {activeTabIndex===index&&<div className="pb-8.4">
+                                    <Overview paragraph={activeTab}/>
+                                </div>
+                                }
+                            </li>
+                        )
+                    }               
                 </ul>
             </div>
         </Wrapper>
     );
 }
-
 export default CompanyOverview;
