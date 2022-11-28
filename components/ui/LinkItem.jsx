08c8c25cfb47fa8543b3b1582e88ea1d613dcd72
Jacob Dev <mercihabam@gmail.com>
2022-11-28 15:12:49 +0200

    fix: add underlined missing property on the linkitem component

--- components/ui/LinkItem.jsx
+++ components/ui/LinkItem.jsx
@@ -1,9 +1,9 @@
 import Image from 'next/image'
 import Link from 'next/link';
 
-const LinkItem = ({children, to}) => {
+const LinkItem = ({children, to, underlined}) => {
   return (
-    <Link href={to || '#'} className="w-max flex items-baseline gap-1 group cursor-pointer">
+    <Link href={to || '#'} className={`w-max flex items-baseline gap-1 group cursor-pointer ${underlined && 'underline'}`}>
         {children}
         <div className="w-max group-hover:translate-x-1 group-hover:-translate-y-1 transition-all">
             <Image width={14} height={14} src="/assets/icons/arrow-link.svg" alt='arrow' />
