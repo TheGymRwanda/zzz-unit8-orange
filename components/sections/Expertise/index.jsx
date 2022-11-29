17cad24a99d5584bc43818de9bf88f5f48229f87
alain-kubwayo <ahkubwayo@gmail.com>
2022-11-29 15:51:21 +0200

    feat: add width to the areas of expertise container and move areas to the data directory

--- components/sections/Expertise/index.jsx
+++ components/sections/Expertise/index.jsx
@@ -1,15 +1,15 @@
 import Wrapper from "../Wrapper";
 import Title from "../../ui/Title";
-import areasOfExpertise from "../../../helpers/data/expertise";
+import areasOfExpertise from "/data/expertise";
 
 const Expertise = () => {
     return (
         <Wrapper>
             <div className="grid place-content-end">
-                <div className="w-full">
+                <div>
                     <Title name="Expertise" />
-                    <ul className="grid grid-cols-1 space-y-2 md:space-y-0 md:grid-cols-2 md:gap-x-6.25 mt-1.75">
-                        {areasOfExpertise.map((area, index) => <li key={index} className="text-2xl -leading-tighter border-b border-primaryGray-50">{area}</li>)}
+                    <ul className="grid grid-cols-1 md:grid-cols-2 gap-x-6.5 mt-1.75 md:mt-6 gap-y-2 md:gap-y-3">
+                        {areasOfExpertise.map((area, index) => <li key={index} className="text-xl md:text-2xl w-290 md:w-310 -leading-tighter border-b border-primaryGray-50">{area}</li>)}
                     </ul>
                 </div>
             </div>
