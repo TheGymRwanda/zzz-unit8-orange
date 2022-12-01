a2717ab4c6e288ba0a7b1337ff38187475fe34bc
Jacob Dev <mercihabam@gmail.com>
2022-12-01 12:15:29 +0200

    fix: added fade animation on project image chane

--- components/sections/Projects/ProjectTitle.jsx
+++ components/sections/Projects/ProjectTitle.jsx
@@ -8,7 +8,7 @@ const ProjectTitle = ({ name, openProjectPage, width, active, index }) => {
       onClick={width > 1024 ? openProjectPage: () =>{}}
     >
       <div className="text-13 leading-16 sm:text-16 sm:leading-18">{name}</div>
-      <div className={"transition-opacity duration-300 " + (active === index ? "opacity-100": "opacity-0")}>
+      <div className={"transition-opacity duration-700 " + (active === index ? "opacity-100": "opacity-0")}>
         <div className="w-full absolute bottom-1 left-1.5">
           <div className="w-full border-b-2 border-black hidden xl:block"></div>
         </div>
--- components/sections/Projects/index.jsx
+++ components/sections/Projects/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import projectsData from '/data/projects'
 import Title from '../../ui/Title'
 import Wrapper from '../Wrapper'
@@ -11,22 +12,27 @@ const Projects = () => {
     <Wrapper>
         <div className="mt-34">
             <Title name="Projects" />
-            <div className="xl:flex justify-between">
-                <div className="flex flex-col relative mt-2.75">
+            <div className="xl:flex justify-between relative mt-2.75">
+                <div className="flex flex-col relative">
                     {
                         projectsData.map((project, index) =>(
                             <ProjectItem {...project} key={index} index={index} setActive={setActive} active={active} />
                         ))
                     }
                 </div>
-                <div
-                    className="hidden xl:block overflow-hidden max-w-202.75 transition-all duration-700 drop-shadow-pr lg:-z-10"
-                >
-                    <div className="rounded-xl overflow-hidden">
-                    <img src={projectsData[active].imgUri} className="max-w-full" alt={projectsData[active].name} />
-                    </div>
-                    <div className="text-2xl mt-2">{projectsData[active].description}</div>
-                </div>
+                {
+                    projectsData.map((project, index) =>(
+                        <div
+                            className={`overflow-hidden absolute right-0 max-w-202.75 transition-all duration-700 drop-shadow-pr lg:-z-10 ${active === index ? 'opacity-100': 'opacity-0'}`}
+                            key={index}
+                        >
+                            <div className="rounded-xl overflow-hidden">
+                            <img src={project.imgUri} className="max-w-full" alt={project.name} />
+                            </div>
+                            <div className="text-2xl mt-2">{project.description}</div>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     </Wrapper>
