7ea68aba47bbd07dc0ecba5182df704321ba4827
Jacob Dev <mercihabam@gmail.com>
2022-12-01 17:05:43 +0200

    fix: keep the title underline visible as long as the project image is active

--- components/sections/Projects/ProjectTitle.jsx
+++ components/sections/Projects/ProjectTitle.jsx
@@ -8,9 +8,9 @@ const ProjectTitle = ({ name, openProjectPage, width, active, index }) => {
       onClick={width > 1024 ? openProjectPage: () =>{}}
     >
       <div className="text-13 leading-16 sm:text-21.5 sm:leading-18">{name}</div>
-      <div className={`transition-opacity duration-700 ${(active === index ? "opacity-100": "opacity-0")}`}>
+      <div className={`transition-opacity duration-500 ${(active === index ? "opacity-100": "opacity-0")}`}>
       <div className="overflow-hidden w-full absolute -bottom-1 left-1.5">
-            <div className="-translate-x-full w-2/4 border-b-2 border-black group-hover:w-full group-hover:translate-x-0 transition-all duration-500 hidden xl:block"></div>
+            <div className={`border-b-2 border-black transition-all duration-500 hidden xl:block ${(active === index ? "w-full translate-x-0": "-translate-x-full w-2/4")}`}></div>
         </div>
         <Image
           src="/assets/icons/arrow-link.svg"
@@ -18,7 +18,6 @@ const ProjectTitle = ({ name, openProjectPage, width, active, index }) => {
           width={14}
           height={14}
           className="hidden xl:block mt-3.5"
-          onClick={width < 1024 ? openProjectPage: () =>{}}
         />
       </div>
     </div>
