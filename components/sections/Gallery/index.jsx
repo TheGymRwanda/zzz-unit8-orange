854746b6a6f33c78c5299bd632bc449aa8afe220
alain-kubwayo <ahkubwayo@gmail.com>
2022-12-01 16:42:21 +0200

    feat: add basic gallery functionality

--- components/sections/Gallery/index.jsx
+++ components/sections/Gallery/index.jsx
@@ -1,12 +1,18 @@
+import { calcLength } from "framer-motion";
 import { useState, useRef, useEffect } from "react";
 import { Stage, Layer, Image } from "react-konva";
 import images from "../../../data/gallery";
 
 const Gallery = () => {
-  const [pictures, setPictures] = useState(images);
   const [image, setImage] = useState(null);
-  const [currentImage, setCurrentImage] = useState(null);
+  const [rectangles, setRectangles] = useState();
+  const [canvasWidth, setCanvasWidth] = useState();
+  const [canvasHeight, setCanvasHeight] = useState();
+
   const imagesRef = useRef([]);
+  const indexRef = useRef(0);
+  const containerRef = useRef();
+
   useEffect(() => {
     setImage({
       image: imagesRef.current[0],
@@ -14,20 +20,47 @@ const Gallery = () => {
       height: images[0].height,
       alt: "gallery image",
     });
+    setCanvasHeight(containerRef.current.clientHeight);
+    setCanvasWidth(containerRef.current.clientWidth);
+
   }, []);
 
   const handleDisplayImage = (e) => {
-    console.log(e);
+    indexRef.current += 1;
+    if (indexRef.current > images.length - 1) indexRef.current = 0;
+    console.log(e.nativeEvent.x, e.nativeEvent.y);
+    setImage({
+      x: e.nativeEvent.offsetX,
+      y: e.nativeEvent.offsetY,
+      image: imagesRef.current[indexRef.current],
+      width: images[indexRef.current].width,
+      height: images[indexRef.current].height
+    });
   };
 
   return (
-    <div className="relative h-[95vh] w-[80%]">
+    <div className="relative h-screen w-full" ref={containerRef}>
       <div
-        className="absolute z-50 w-full h-full"
-        onMouseMove={(e) => handleDisplayImage(e)}
-      ></div>
+        className="absolute z-50 w-full h-full border-4 border-sky-900"
+        onMouseMove={handleDisplayImage}
+      >
+        <Stage width={canvasWidth} height={canvasHeight}>
+        <Layer>
+          {image && (
+            <Image
+              image={image.image}
+              width={image.width}
+              height={image.height}
+              x={image.x}
+              y={image.y}
+              alt="gallery image"
+            />
+          )}
+        </Layer>
+      </Stage>
+      </div>
       <div className="h-0 w-0 opacity-0">
-        {pictures.map((picture, i) => (
+        {images.map((picture, i) => (
           // eslint-disable-next-line @next/next/no-img-element
           <img
             src={picture.url}
@@ -37,18 +70,7 @@ const Gallery = () => {
           />
         ))}
       </div>
-      <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer>
-          {image && (
-            <Image
-              image={image.image}
-              width={image.width}
-              height={image.height}
-              alt="hshshs"
-            />
-          )}
-        </Layer>
-      </Stage>
+      {/*  */}
     </div>
   );
 };
