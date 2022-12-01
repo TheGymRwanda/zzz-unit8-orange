fcc2ab00a5e1cceaf4a8abd12752981761c1583c
Jacob Dev <mercihabam@gmail.com>
2022-12-01 15:25:42 +0200

    feat: make projects images stick on scroll

--- components/sections/Projects/index.jsx
+++ components/sections/Projects/index.jsx
@@ -10,29 +10,35 @@ const Projects = () => {
 
   return (
     <Wrapper>
-        <div className="mt-34">
+        <div className="my-34 xl:pb-[432px]">
             <Title name="Projects" />
-            <div className="xl:flex justify-between relative mt-2.75">
-                <div className="flex flex-col relative">
+            <div className="xl:flex items-start justify-between mt-2.75">
+                <div className="xl:h-28">
+                    <div className="xl:absolute">
+                        <div className="flex flex-col relative">
+                            {
+                                projectsData.map((project, index) =>(
+                                    <ProjectItem {...project} key={index} index={index} setActive={setActive} active={active} />
+                                ))
+                            }
+                        </div>
+                    </div>
+                </div>
+                <div className="hidden xl:block sticky w-full top-10 bg-red-600">
                     {
                         projectsData.map((project, index) =>(
-                            <ProjectItem {...project} key={index} index={index} setActive={setActive} active={active} />
+                            <div
+                                className={`overflow-hidden absolute right-0 max-w-202.75 transition-all duration-700 drop-shadow-pr lg:-z-10 ${active === index ? 'opacity-100': 'opacity-0'}`}
+                                key={index}
+                            >
+                                <div className="rounded-xl overflow-hidden">
+                                <img src={project.imgUri} className="max-w-full" alt={project.name} />
+                                </div>
+                                <div className="text-2xl mt-2">{project.description}</div>
+                            </div>
                         ))
                     }
                 </div>
-                {
-                    projectsData.map((project, index) =>(
-                        <div
-                            className={`overflow-hidden absolute right-0 max-w-202.75 transition-all duration-700 drop-shadow-pr lg:-z-10 ${active === index ? 'opacity-100': 'opacity-0'}`}
-                            key={index}
-                        >
-                            <div className="rounded-xl overflow-hidden">
-                            <img src={project.imgUri} className="max-w-full" alt={project.name} />
-                            </div>
-                            <div className="text-2xl mt-2">{project.description}</div>
-                        </div>
-                    ))
-                }
             </div>
         </div>
     </Wrapper>
