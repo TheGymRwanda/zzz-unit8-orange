7b5f969c7a1ca2fa75724c120e9417c127586db6
alain-kubwayo <ahkubwayo@gmail.com>
2022-12-06 14:49:47 +0200

    fix: add margin bottom to the project title on mobile and tablet breakpoints

--- components/sections/Projects/ProjectTitle.jsx
+++ components/sections/Projects/ProjectTitle.jsx
@@ -4,7 +4,7 @@ const ProjectTitle = ({ name, openProjectPage, width, active, index }) => {
 
   return (
     <div
-      className="cursor-pointer relative group flex items-start"
+      className="cursor-pointer relative group flex items-start mb-5.1575 md:mb-4.5 lg:mb-2.9 xl:mb-0"
       onClick={width > 1024 ? openProjectPage: () =>{}}
     >
       <div className="text-13 leading-16 sm:text-21.5 sm:leading-18">{name}</div>
