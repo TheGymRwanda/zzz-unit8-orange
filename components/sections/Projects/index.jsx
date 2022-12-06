8223f41dc58b4c2db1d94e3f1f7c1b2ab6b70a3c
clever-cr <cleverumuhuza@gmail.com>
2022-12-06 17:27:14 +0200

    fix: Add image height on tablet breakpoint

--- components/sections/Projects/index.jsx
+++ components/sections/Projects/index.jsx
@@ -9,37 +9,45 @@ const Projects = () => {
 
   return (
     <Wrapper>
-        <div className="my-34 xl:pb-137">
-            <Title name="Projects" />
-            <div className="xl:flex items-start justify-between mt-2.75">
-                <div className="xl:h-48">
-                    <div className="xl:absolute">
-                        <div className="flex flex-col relative xl:gap-6.5">
-                            {
-                                projectsData.map((project, index) =>(
-                                    <ProjectItem {...project} key={index} index={index} setActive={setActive} active={active} />
-                                ))
-                            }
-                        </div>
-                    </div>
-                </div>
-                <div className="hidden xl:block sticky w-full top-10 bg-red-600">
-                    {
-                        projectsData.map((project, index) =>(
-                            <div
-                                className={`overflow-hidden absolute right-0 max-w-202.75 transition-all duration-700 drop-shadow-pr lg:-z-10 ${active === index ? "opacity-100": "opacity-0"}`}
-                                key={index}
-                            >
-                                <div className="rounded-xl overflow-hidden">
-                                <img src={project.imgUri} className="max-w-full" alt={project.name} />
-                                </div>
-                                <div className="text-2xl mt-2">{project.description}</div>
-                            </div>
-                        ))
-                    }
-                </div>
+      <div className="my-34 xl:pb-137">
+        <Title name="Projects" />
+        <div className="xl:flex items-start justify-between mt-2.75">
+          <div className="xl:h-48">
+            <div className="xl:absolute">
+              <div className="flex flex-col relative xl:gap-6.5">
+                {projectsData.map((project, index) => (
+                  <ProjectItem
+                    {...project}
+                    key={index}
+                    index={index}
+                    setActive={setActive}
+                    active={active}
+                  />
+                ))}
+              </div>
             </div>
+          </div>
+          <div className="hidden xl:block sticky w-full top-10 bg-red-600">
+            {projectsData.map((project, index) => (
+              <div
+                className={`overflow-hidden absolute right-0 max-w-202.75 transition-all duration-700 drop-shadow-pr lg:-z-10 ${
+                  active === index ? "opacity-100" : "opacity-0"
+                }`}
+                key={index}
+              >
+                <div className="rounded-xl overflow-hidden">
+                  <img
+                    src={project.imgUri}
+                    className="max-w-full"
+                    alt={project.name}
+                  />
+                </div>
+                <div className="text-2xl mt-2">{project.description}</div>
+              </div>
+            ))}
+          </div>
         </div>
+      </div>
     </Wrapper>
   );
 };
