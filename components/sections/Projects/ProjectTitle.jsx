ae8f15ee27713d1fd6386a433976525b43afc13e
Jacob Dev <mercihabam@gmail.com>
2022-12-20 15:30:12 +0200

    fix: add duration of 300ms to the project's title arrow

--- components/sections/Projects/ProjectTitle.jsx
+++ components/sections/Projects/ProjectTitle.jsx
@@ -11,12 +11,12 @@ const ProjectTitle = ({ name, openProjectPage, width, active, index }) => {
       <div className="overflow-hidden w-full absolute -bottom-1.5 left-0">
           <div className={`border-b-2 border-black transition-all duration-300 hidden xl:block ${(active === index ? "w-full translate-x-0": "-translate-x-full w-0")}`}></div>
       </div>
-      <div className={`mt-1.5 transition-all duration-300 translate-y-0.75 ${(active === index ? "opacity-100 translate-y-0": "opacity-0")}`}>
+      <div className={`mt-1.5 transition-all delay-300 duration-300 translate-x-1 translate-y-0 ${(active === index ? "opacity-100 translate-y-0.75": "opacity-0")}`}>
         <Image
           src="/assets/icons/arrow-link.svg"
           alt="arrow"
-          width={14}
-          height={14}
+          width={13}
+          height={13}
           className="hidden xl:block"
         />
       </div>
