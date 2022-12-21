53ca12bc847c6e3201f73db4dcbc6150a0149b59
Odile078 <ouwimpuhwe620@daviscollege.com>
2022-12-21 15:02:45 +0200

    refactor: removed large desktop screen padding

--- components/Wrapper/index.jsx
+++ components/Wrapper/index.jsx
@@ -1,8 +1,6 @@
 const Wrapper = ({ children }) => {
   return (
-    <div className="px-5 sm:px-9 xl:px-16 2xl:px-35.25 2xl:mx-auto max-w-9xl">
-      {children}
-    </div>
+    <div className="px-5 sm:px-9 xl:px-16 xl:mx-auto max-w-9xl">{children}</div>
   );
 };
 
