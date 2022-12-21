3f61767a397e428f94320a960afd4002a98c3c94
Liliane Iradukunda <Lialsina03@gmail.com>
2022-12-21 15:09:14 +0200

    refactor: replace the spaces with padding and change contact to be a link

--- components/Layout/Header.jsx
+++ components/Layout/Header.jsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import Wrapper from "../sections/Wrapper";
 import LinkItem from "../ui/LinkItem";
 
@@ -6,20 +5,22 @@ const Header = () => {
   return (
     <Wrapper>
       <div className="flex flex-col justify-between gap-5 py-12 text-xl lg:flex-row sm:text-2xl -tracking-tighter">
-        <div>Ape Unit</div>
+        <h1>Ape Unit</h1>
         <div className="max-w-189.25 xl:max-w-201.5">
           Unit➇ is a pioneering technology company specialing in decentralised
           technologies that creates end-to-end digital experiences for protocols
-          including{" "}
-          <span className="text-purple">
+          including
+          <span className="text-purple px-1">
             Ethereum, Tezos, Near, Algorand, Celo
-          </span>{" "}
+          </span>
           and more. Its team of 2,500+ digital specialists across 30+ locations
           on 5 continents delivers pioneering work on a global scale with a
-          boutique culture.{" "}
-          <LinkItem underlined inline>
-            Contact us
-          </LinkItem>
+          boutique culture.
+          <span className="pl-1">
+            <LinkItem to="mailto:web3@apeunit.com" underlined inline>
+              Contact us
+            </LinkItem>
+          </span>
         </div>
       </div>
     </Wrapper>
