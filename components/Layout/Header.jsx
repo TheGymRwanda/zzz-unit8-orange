05deee4692890ada3b49d29d9c02efe486db5e04
Odile078 <ouwimpuhwe620@daviscollege.com>
2022-12-22 15:52:33 +0200

    fix: add gap for mobile device

--- components/Layout/Header.jsx
+++ components/Layout/Header.jsx
@@ -4,7 +4,7 @@ import LinkItem from "../ui/LinkItem";
 const Header = () => {
   return (
     <Wrapper>
-      <div className="flex flex-col justify-between gap-4.2 sm:gap-5.5 pt-6.5 sm:pt-12 text-xl lg:flex-row sm:text-2xl -tracking-tighter sm:tracking-normal">
+      <div className="flex flex-col justify-between pt-6.5 sm:pt-12 text-xl lg:flex-row sm:text-2xl -tracking-tighter sm:tracking-normal gap-4.2 sm:gap-5.5 ">
         <h1>Ape Unit</h1>
         <div className="max-w-189.25 xl:max-w-201.5 md:pt-0">
           Unit➇ is a pioneering technology company specialing in decentralised
